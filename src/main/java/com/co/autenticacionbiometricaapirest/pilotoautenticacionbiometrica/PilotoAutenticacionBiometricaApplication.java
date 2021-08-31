6cc2d2dde4fe90b298654ce04337fc166b5d892b
Camilo <ca-milin15@hotmail.com>
2021-08-30 20:31:22 -0500

    Commit inicial. Estructura del proyecto

---
+++ src/main/java/com/co/autenticacionbiometricaapirest/pilotoautenticacionbiometrica/PilotoAutenticacionBiometricaApplication.java
@@ -0,0 +1,13 @@
+package com.co.autenticacionbiometricaapirest.pilotoautenticacionbiometrica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotoAutenticacionBiometricaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilotoAutenticacionBiometricaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/autenticacionbiometricaapirest/pilotoautenticacionbiometrica/PilotoAutenticacionBiometricaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.autenticacionbiometricaapirest.pilotoautenticacionbiometrica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilotoAutenticacionBiometricaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
