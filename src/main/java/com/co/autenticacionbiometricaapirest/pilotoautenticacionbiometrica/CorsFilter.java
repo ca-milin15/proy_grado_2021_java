4f8936fad1d30be5913730295c0743a28cb5cdb9
Camilo <ca-milin15@hotmail.com>
2021-09-29 23:37:59 -0500

    Integrar apartado registro de la APP con API rest

---
+++ src/main/java/com/co/autenticacionbiometricaapirest/pilotoautenticacionbiometrica/CorsFilter.java
@@ -0,0 +1,28 @@
+package com.co.autenticacionbiometricaapirest.pilotoautenticacionbiometrica;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CorsFilter implements Filter{
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+			throws IOException, ServletException {
+		 HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+        filterChain.doFilter(servletRequest, servletResponse);
+	}
+
+}
