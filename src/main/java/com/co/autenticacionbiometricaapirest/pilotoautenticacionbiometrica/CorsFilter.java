0b2c733e068d988e6e86ad9480822d3117305779
Camilo <ca-milin15@hotmail.com>
2021-11-05 18:21:25 -0500

    Agregar configuracion en la peticion de autenticacion

--- src/main/java/com/co/autenticacionbiometricaapirest/pilotoautenticacionbiometrica/CorsFilter.java
+++ src/main/java/com/co/autenticacionbiometricaapirest/pilotoautenticacionbiometrica/CorsFilter.java
@@ -21,7 +21,7 @@ public class CorsFilter implements Filter{
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With, ResponseType");
         filterChain.doFilter(servletRequest, servletResponse);
 	}
 
--- src/main/java/com/co/autenticacionbiometricaapirest/pilotoautenticacionbiometrica/autenticacion/autenticacion/controller/AutenticacionController.java
+++ src/main/java/com/co/autenticacionbiometricaapirest/pilotoautenticacionbiometrica/autenticacion/autenticacion/controller/AutenticacionController.java
@@ -1,10 +1,13 @@
 package com.co.autenticacionbiometricaapirest.pilotoautenticacionbiometrica.autenticacion.autenticacion.controller;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
 
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -33,9 +36,13 @@ public class AutenticacionController {
 		try {
 			var pathFile = autenticacionService.autenticar(fotografia);
 			var file = pathFile.toFile();
-			return ResponseEntity.ok().contentLength(file.length()).contentType(MediaType.APPLICATION_OCTET_STREAM)
+			return ResponseEntity.ok()
+					.header(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, HttpHeaders.CONTENT_DISPOSITION)
+					.header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=".concat(file.getName()))
+					.contentLength(file.length())
+					.contentType(MediaType.APPLICATION_OCTET_STREAM)
 					.body(new InputStreamResource(new FileInputStream(file)));
-		} catch (FileNotFoundException e) {
+		} catch (Exception e) {
 			log.error(ExceptionUtils.getMessage(e));
 			throw new ArchivoProcesoDescargaRuntimeException(MessageStaticClass.ERR_CONST_ARCHIVO_DESCARGA.getMensaje());
 		}
