021693e7a71e3a624fef45b19effb85c430da8d6
Camilo <ca-milin15@hotmail.com>
2021-11-05 00:17:58 -0500

    Modificar forma de cargar y descargas objetos de S3

--- src/main/java/com/co/autenticacionbiometricaapirest/pilotoautenticacionbiometrica/autenticacion/almacenamiento/S3AlmacenamientoServiceImpl.java
+++ src/main/java/com/co/autenticacionbiometricaapirest/pilotoautenticacionbiometrica/autenticacion/almacenamiento/S3AlmacenamientoServiceImpl.java
@@ -4,9 +4,15 @@ import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.stereotype.Service;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.co.autenticacionbiometricaapirest.pilotoautenticacionbiometrica.config.AWSPropiedadesSistema;
 import com.co.autenticacionbiometricaapirest.pilotoautenticacionbiometrica.utilidades.enums.MessageStaticClass;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -14,11 +20,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.transfer.s3.S3ClientConfiguration;
-import software.amazon.awssdk.transfer.s3.S3TransferManager;
 
 @Slf4j
 @Service
@@ -31,17 +32,13 @@ public class S3AlmacenamientoServiceImpl implements AlmacenamientoService {
 	@Override
 	public String cargarObjeto(String bucketName, String fileName, File fotografia) {
 		try {
-			var awsCredentials = AwsBasicCredentials.create(
+			var awsCredentials = new BasicAWSCredentials(
 					propiedadesSistema.getConfig().getAk(), 
 					propiedadesSistema.getConfig().getSk()
 			);
-			var s3ClientConfig = S3ClientConfiguration.builder()
-						.region(Region.US_EAST_1)
-						.credentialsProvider(StaticCredentialsProvider.create(awsCredentials)).build();
-			var s3TransferManager = S3TransferManager.builder().s3ClientConfiguration(s3ClientConfig).build();
-			s3TransferManager.upload(builder -> 
-				builder.putObjectRequest(poq -> 
-					poq.bucket(bucketName).key(fileName.concat(fotografia.getName()))).source(Path.of(fotografia.getPath())));
+			var s3ClientConfig = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+						.withRegion(Regions.US_EAST_1).build();
+			s3ClientConfig.putObject(bucketName, fileName.concat(fotografia.getName()), fotografia);
 			return new StringBuilder().append(bucketName).append("/").append(fileName).append(fotografia.getName()).toString();
 		} catch (Exception e) {
 			log.error(ExceptionUtils.getMessage(e));
@@ -53,12 +50,16 @@ public class S3AlmacenamientoServiceImpl implements AlmacenamientoService {
 	@Override
 	public String descargarObjeto(String nombreRepositorioS3, String nombreArchivo) {
 		try {
-			var s3TransferManager = S3TransferManager.create();
-			var download = s3TransferManager.download(builder -> 
-				builder.getObjectRequest(poq -> 
-					poq.bucket(nombreRepositorioS3).key(nombreArchivo)).destination(Paths.get(System.getProperty("java.io.tmpdir"), nombreArchivo)));
-			log.info("descargarObjeto: ".concat(objectMapper.writeValueAsString(download.completionFuture().join().response())));
-			return null;
+			var awsCredentials = new BasicAWSCredentials(
+					propiedadesSistema.getConfig().getAk(), 
+					propiedadesSistema.getConfig().getSk()
+			);
+			var s3ClientConfig = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+						.withRegion(Regions.US_EAST_1).build();
+			var object = s3ClientConfig.getObject(nombreRepositorioS3, nombreArchivo);
+			var pathFileToDownload = Paths.get(System.getProperty("java.io.tmpdir"), nombreArchivo);
+			FileUtils.copyInputStreamToFile(object.getObjectContent(), pathFileToDownload.toFile());
+			return pathFileToDownload.toString();
 		} catch (Exception e) {
 			log.error(ExceptionUtils.getMessage(e));
 			log.error(ExceptionUtils.getRootCauseMessage(e));
